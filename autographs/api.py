9b8104ef4ccdf98d30c09c79524d5a71fdff9eb0
a.bogdan <aleksandra.bogdan30@gmail.com>
2023-01-13 11:54:08 +0100

    [dev] add additional actions to persion viewset

--- autographs/api.py
+++ autographs/api.py
@@ -1,19 +1,59 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Person, Address, Letter
-from .serializers import PersonSerializer, AddressSerializer, LetterSerializer
+from .serializers import MiniPersonSerializer, BasicPersonSerializer, ExtendedPersonSerializer
+from .serializers import MiniAddressSerializer, BasicAddressSerializer
+from .serializers import MiniLetterSerializer, BasicLetterSerializer
 
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+    serializer_class = BasicPersonSerializer
+
+    @action(detail=True)
+    def letters(self, request, **kwargs):
+        person = self.get_object()
+        letters = person.letters
+
+        serializer = BasicLetterSerializer(letters, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def letters_with_response(self, request, **kwargs):
+        person = self.get_object()
+        letters = person.letters.filter(is_responded=True)
+
+        serializer = BasicLetterSerializer(letters, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def letters_without_response(self, request, **kwargs):
+        person = self.get_object()
+        letters = person.letters.filter(is_responded=False)
+
+        serializer = BasicLetterSerializer(letters, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def addresses(self, request, **kwargs):
+        person = self.get_object()
+        addresses = person.addresses
+
+        serializer = BasicAddressSerializer(addresses, many=True)
+
+        return Response(serializer.data)
 
 
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
-    serializer_class = AddressSerializer
+    serializer_class = BasicAddressSerializer
 
 
 class LetterViewSet(viewsets.ModelViewSet):
     queryset = Letter.objects.all()
-    serializer_class = LetterSerializer
+    serializer_class = BasicLetterSerializer
--- autographs/serializers.py
+++ autographs/serializers.py
@@ -3,30 +3,46 @@ from rest_framework import serializers
 from .models import Person, Address, Letter
 
 
-class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Address
-        fields = '__all__'
-
-
-class PersonSerializer(serializers.ModelSerializer):
-    addresses = AddressSerializer(many=True)
-
-    class Meta:
-        model = Person
-        fields = '__all__'
-
-
 class MiniPersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = ['first_name', 'last_name']
 
 
-class LetterSerializer(serializers.ModelSerializer):
-    # address = AddressSerializer(many=False)
-    # to_whom = MiniPersonSerializer(many=False)
+class MiniAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        exclude = ['person', ]
 
+
+class MiniLetterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Letter
+        exclude = ['to_whom', 'address']
+
+
+class BasicPersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = '__all__'
+
+
+class BasicAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class BasicLetterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Letter
+        fields = '__all__'
+
+
+class ExtendedPersonSerializer(serializers.ModelSerializer):
+    addresses = MiniAddressSerializer(many=True)
+    letters = MiniLetterSerializer(many=True)
+
+    class Meta:
+        model = Person
         fields = '__all__'
